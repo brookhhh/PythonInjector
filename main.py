bc57dcc8b9ca4111f78d6b8a3838a37cf52a1644
brook <81716228+brooke-a@users.noreply.github.com>
2022-06-05 11:51:36 +0100

    Yea yea

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ for file_name in os.listdir(os.path):
     time.sleep(100)
 
     # injecting dll
-    if pymem.open_process_from_name('Minecraft.Bedrock'):
+    if pymem.open_process_from_name('Minecraft.Bedrock', 'Minecraft.Bedrock.exe'):
+        print("Process found!")
         print("Injecting...")
-        pymem.injectDll(file_name.endswith('.dll') in "\\PythonInjectorMCBE")
-        print("Should be injected!")
+        if (pymem.injectDll(file_name.endswith('.dll') in "\\PythonInjectorMCBE") == true): # location of dll (this will inject all dlls in this folder probably)
+          print("Should be injected!")
